2a14ffcd9cb5e95dc21bd59c4d424956a9d8f766
Samuel Guedes <sguedes46@gmail.com>
2022-05-26 23:38:46 -0300

    Criação de todos os Controllers.

---
+++ Hemolink/Hemolink/Controllers/AgendaController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hemolink.Models;
+
+namespace Hemolink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendaController : ControllerBase
+    {
+        private readonly _DbContext _context;
+
+        public AgendaController(_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Agenda
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agenda>>> Getagenda()
+        {
+          if (_context.agenda == null)
+          {
+              return NotFound();
+          }
+            return await _context.agenda.ToListAsync();
+        }
+
+        // GET: api/Agenda/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Agenda>> GetAgenda(int id)
+        {
+          if (_context.agenda == null)
+          {
+              return NotFound();
+          }
+            var agenda = await _context.agenda.FindAsync(id);
+
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            return agenda;
+        }
+
+        // PUT: api/Agenda/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAgenda(int id, Agenda agenda)
+        {
+            if (id != agenda.IdAgendamento)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(agenda).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgendaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Agenda
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Agenda>> PostAgenda(Agenda agenda)
+        {
+          if (_context.agenda == null)
+          {
+              return Problem("Entity set '_DbContext.agenda'  is null.");
+          }
+            _context.agenda.Add(agenda);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgenda", new { id = agenda.IdAgendamento }, agenda);
+        }
+
+        // DELETE: api/Agenda/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAgenda(int id)
+        {
+            if (_context.agenda == null)
+            {
+                return NotFound();
+            }
+            var agenda = await _context.agenda.FindAsync(id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            _context.agenda.Remove(agenda);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AgendaExists(int id)
+        {
+            return (_context.agenda?.Any(e => e.IdAgendamento == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Hemolink/Hemolink/Controllers/QuestionarioController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hemolink.Models;
+
+namespace Hemolink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionarioController : ControllerBase
+    {
+        private readonly _DbContext _context;
+
+        public QuestionarioController(_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questionario
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Questionario>>> Getquestionario()
+        {
+          if (_context.questionario == null)
+          {
+              return NotFound();
+          }
+            return await _context.questionario.ToListAsync();
+        }
+
+        // GET: api/Questionario/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Questionario>> GetQuestionario(int id)
+        {
+          if (_context.questionario == null)
+          {
+              return NotFound();
+          }
+            var questionario = await _context.questionario.FindAsync(id);
+
+            if (questionario == null)
+            {
+                return NotFound();
+            }
+
+            return questionario;
+        }
+
+        // PUT: api/Questionario/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestionario(int id, Questionario questionario)
+        {
+            if (id != questionario.IdFormulario)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(questionario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Questionario
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Questionario>> PostQuestionario(Questionario questionario)
+        {
+          if (_context.questionario == null)
+          {
+              return Problem("Entity set '_DbContext.questionario'  is null.");
+          }
+            _context.questionario.Add(questionario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestionario", new { id = questionario.IdFormulario }, questionario);
+        }
+
+        // DELETE: api/Questionario/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestionario(int id)
+        {
+            if (_context.questionario == null)
+            {
+                return NotFound();
+            }
+            var questionario = await _context.questionario.FindAsync(id);
+            if (questionario == null)
+            {
+                return NotFound();
+            }
+
+            _context.questionario.Remove(questionario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionarioExists(int id)
+        {
+            return (_context.questionario?.Any(e => e.IdFormulario == id)).GetValueOrDefault();
+        }
+    }
+}
